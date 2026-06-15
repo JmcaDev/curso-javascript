a64ea5f71ffac3ee8759cf51da149827d4d9d5b5
JmcaDev <jmcavzla@gmail.com>
2026-06-15 09:33:32 -0400

    Variables, Strings, numbers, operators, booleans and objects

---
+++ 01-Introduccion/js/app.js
@@ -0,0 +1,14 @@
+//Comentario de una linea
+
+/**
+ * Comentario de varias lineas
+*/
+
+//Pregunta al usuario su nombre
+const nombre = prompt("Cual es tu nombre")
+
+//Toma el nombre y lo muestra en pantalla
+document.querySelector(".contenido").innerHTML = `${nombre} está aprendiendo javascript moderno`
+
+//Asigna el valor hacia la variable producto
+const producto = "Monitor 24 pulgadas"
---
+++ 03-Strings/js/01-app.js
@@ -0,0 +1,6 @@
+const producto = "Monitor de 20\"";
+const producto2 = String('Monitor de 24"')
+
+const producto3 = new String('Monitor de 27 pulgadas')
+
+console.log(producto)
