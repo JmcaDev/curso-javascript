9401b534a4ea5d34ff9897dd01d9f6824be738f3
JmcaDev <jmcavzla@gmail.com>
2026-06-19 22:04:50 -0400

    Reconstruc shopping cart, localstorage, shopping cart with localstorage, dates and prototypes

---
+++ 18-LocalStorage/js/03-app.js
@@ -0,0 +1,9 @@
+localStorage.removeItem("nombre")
+
+//Actualizar un registro
+const mesesArray = JSON.parse(localStorage.getItem("meses"))
+mesesArray.push("Nuevo mes")
+console.log(mesesArray)
+localStorage.setItem("meses", JSON.stringify(mesesArray))
+
+localStorage.clear()
